702664bae7c826b5391e6f78829d2fe93268239e
Suzukiti-1029 <h40396suzujun@gmail.com>
2026-01-25 13:28:38 +0000

    feat: `$5 * 2 = $10`

---
+++ java/src/main/java/com/example/money/Dollar.java
@@ -0,0 +1,13 @@
+package com.example.money;
+
+class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
---
+++ java/src/test/java/com/example/money/MoneyTest.java
@@ -0,0 +1,14 @@
+package com.example.money;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
+}
