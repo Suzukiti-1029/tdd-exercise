95d2b280e1f7aefd9ee63e6cbce2bda24a463b99
Suzukiti-1029 <h40396suzujun@gmail.com>
2026-01-25 22:11:01 +0000

    feat: ディレクト構成変更、デフォルトファイル削除

--- java/src/main/java/money/Dollar.java
+++ java/src/main/java/money/Dollar.java
@@ -1,4 +1,4 @@
-package com.example.money;
+package money;
 
 class Dollar {
     int amount;
--- java/src/test/java/money/MoneyTest.java
+++ java/src/test/java/money/MoneyTest.java
@@ -1,4 +1,4 @@
-package com.example.money;
+package money;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
