84c32e2e766bfc444baffd6fca51409878344c5f
Suzukiti-1029 <h40396suzujun@gmail.com>
2026-01-28 22:10:04 +0000

    feat: `equals()`メソッド

--- java/src/test/java/money/MoneyTest.java
+++ java/src/test/java/money/MoneyTest.java
@@ -13,4 +13,10 @@ public class MoneyTest {
         product = five.times(3);
         assertThat(product.amount).isEqualTo(15);
     }
+
+    @Test
+    public void testEquality() {
+        assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
+        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
+    }
 }
